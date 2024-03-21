19dcdc044d8ae6c3d5fa3f2a7c230e2b46fa1203
Tyopo131 <samecob62@gmail.com>
2024-03-21 17:25:11 +0000

    Added missing features

--- Password/Password.cpp
+++ Password/Password.cpp
@@ -2,7 +2,6 @@
 #include <filesystem>
 #include <fstream>
 #include <cstdlib>
-std::istream* read_file = nullptr;
 
 using namespace std::filesystem;
 int main() {
@@ -35,6 +34,44 @@ int main() {
 		fileWrite << password;
 		passwordValid = true;
 		system("CLS");
+		fileWrite.close();
+	}
+	else {
+	enterPassword:
+		std::string passwordEntered;
+		std::ifstream read_password(fileName);
+		std::string password; read_password >> password;
+		std::cout << "Enter password: "; std::cin >> passwordEntered;
+		if (password == passwordEntered) {
+			std::cout << "Correct!\n\n";
+		choosePassword2:
+			std::cout << "Passwords must have 1 special character and must be at least 5 letters long.\n\nEnter new password: ";
+			read_password.close();
+			std::cin >> password; // We're not using password anymore, might as well save memory by reusing it.
+			bool passwordValid = false;
+			if (password.length() >= 5) {
+
+				for (int timer = 0; timer < password.length(); timer++) {
+					if ((password[timer] > 32 && password[timer] < 48) || (password[timer] > 57 && password[timer] < 65)) {
+						passwordValid = true;
+						break;
+					}
+
+				}
+			}
+			if (passwordValid) {
+				std::ofstream write_password(fileName);
+				write_password << password;
+				write_password.close();
+			}
+			else goto choosePassword2;
+
+		}
+		else {
+			std::cout << "Incorrect!\n\n";
+			goto enterPassword;
+	}
+
 
 	}
 }
